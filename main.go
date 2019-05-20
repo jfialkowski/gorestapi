693d3bba230b67ed78c1add0f5743a73af4387d5
jfialkowski <noreply@prgrmr.io>
2019-05-20 16:09:52 -0400

    Added main.go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Just a Homepage, nothing really to see here!")
+	fmt.Println("Endpoint Accessed: homePage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":9999", nil))
+}
+
+func main() {
+	handleRequests()
+}
