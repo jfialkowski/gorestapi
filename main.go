3f35d1e635028bf39835dac40663ce141709258b
jfialkowski <noreply@prgrmr.io>
2019-05-20 16:59:58 -0400

    main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Just a Homepage, nothing really to see here!")
+	fmt.Fprintf(w, "Just a Homepage, nothing really to see here! ")
 	fmt.Println("Endpoint Accessed: homePage")
 }
 
