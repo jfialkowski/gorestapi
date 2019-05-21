974018be7c5793c3cf849fb29e68f30e481a9ae6
jfialkowski <noreply@prgrmr.io>
2019-05-21 08:10:57 -0400

    Fixed Export on Struct by capitalizing first letter of each tag. The package encoding/json will ignore unexported tags...Learn something new everyday.

--- main.go
+++ main.go
@@ -9,35 +9,33 @@ import (
 
 // Employee Struct
 type Employee struct {
-	firstName  string `json:"firstName"`
-	lastName   string `json:"lastName"`
-	title      string `json:"title"`
-	department string `json:"department"`
+	FirstName  string `json:"firstName"`
+	LastName   string `json:"lastName"`
+	Title      string `json:"title"`
+	Department string `json:"department"`
 }
 
-var Employees []Employee
+type Employees []Employee
 
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Just a Homepage, nothing really to see here! ")
 	fmt.Println("Endpoint Accessed: home ")
 }
 
-func returnAllEmployees(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Accessed: returnAllEmployees")
-	json.NewEncoder(w).Encode(Employees)
-}
-
 func handleRequests() {
 	http.HandleFunc("/", home)
-	http.HandleFunc("/employees", returnAllEmployees)
+	http.HandleFunc("/employees", EmployeesIndex)
 	log.Fatal(http.ListenAndServe(":9999", nil))
 }
 
-func main() {
-	//Employees = []Employee{}
-	//Employees array to be used in main function
-	Employees = append(Employees, Employee{firstName: "John", lastName: "Doe", title: "Senior Developer", department: "Information Technology"})
-	Employees = append(Employees, Employee{firstName: "Jane", lastName: "Smith", title: "Manager", department: "Information Technology"})
+func EmployeesIndex(w http.ResponseWriter, r *http.Request) {
+	employees := Employees{
+		Employee{FirstName: "John", LastName: "Doe", Title: "Senior Developer", Department: "Information Technology"},
+		Employee{FirstName: "Jane", LastName: "Smith", Title: "Manager", Department: "Information Technology"},
+	}
+	json.NewEncoder(w).Encode(employees)
+}
 
+func main() {
 	handleRequests()
 }
