51807e182c7b1605e92940901a901d22584ac3bf
jfialkowski <noreply@prgrmr.io>
2019-05-20 22:13:56 -0400

    not done yet

--- main.go
+++ main.go
@@ -6,16 +6,34 @@ import (
 	"net/http"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
+// Declare struct of Employee Data
+type Employee struct {
+	firstName   string `json:"firstName"`
+	lastName    string `json:"lastName"`
+	title       string `json:"title"`
+	deptartment string `json:"department"`
+}
+
+// Declare a global Employee array to be used in main function
+var Employees []Employee
+
+func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Just a Homepage, nothing really to see here! ")
-	fmt.Println("Endpoint Accessed: homePage ")
+	fmt.Println("Endpoint Accessed: home ")
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
+	http.HandleFunc("/", home)
 	log.Fatal(http.ListenAndServe(":9999", nil))
 }
 
 func main() {
+
+	//Some dummy Data
+
+	employees := Employees{
+		Employee{firstName: "John", lastName: "Doe", title: "Senior Developer", department: "Information Technology"},
+		Employee{firstName: "Jane", lastName: "Smith", title: "Manager", department: "Information Technology"},
+	}
 	handleRequests()
 }
