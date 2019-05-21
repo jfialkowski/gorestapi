c45f6c9115df95f116916467d92850a3eba6673f
jfialkowski <noreply@prgrmr.io>
2019-05-21 09:06:04 -0400

    Split things up a bit more, for your health

--- Handlers.go
+++ Handlers.go
@@ -8,16 +8,16 @@ import (
 
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Just a Homepage, nothing really to see here! ")
-	fmt.Println("Endpoint Accessed: home ")
 }
 
 func EmployeesIndex(w http.ResponseWriter, r *http.Request) {
 	employees := Employees{
-		Employee{FirstName: "John", LastName: "Doe", Title: "Senior Developer", Department: "Information Technology"},
+		Employee{FirstName: "John", LastName: "Doe", Title: "Developer", Department: "Information Technology"},
 		Employee{FirstName: "Jane", LastName: "Smith", Title: "Manager", Department: "Information Technology"},
 	}
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(employees); err != nil {
 		panic(err)
 	}
 }
-
