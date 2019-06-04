0d8339234f6556083b62a3c0a969be46e4961dd0
jfialkowski <noreply@prgrmr.io>
2019-06-03 21:52:28 -0400

    Unfix for testing

--- Database.go
+++ Database.go
@@ -2,24 +2,22 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
-	"log"
 
-	"github.com/eduardbcom/gonfig"
+	//"github.com/eduardbcom/gonfig"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type Config struct {
-	DbConfig struct {
-		Host         string `json:"host"`
-		DatabaseName string `json:databasename`
-		Port         string `json:"port"`
-		Username     string `json:username`
-		Password     string `json:password`
-	} `json:"dbConfig"`
-	Name string `json:"name"`
-}
+// type Config struct {
+// 	DbConfig struct {
+// 		Host         string `json:"host"`
+// 		DatabaseName string `json:databasename`
+// 		Port         string `json:"port"`
+// 		Username     string `json:username`
+// 		Password     string `json:password`
+// 	} `json:"dbConfig"`
+// 	Name string `json:"name"`
+// }
 
 func connectDB(username string, password string, host string, port string, dbname string) {
 	db, err := sql.Open("mysql", username+":"+password+"@tcp("+host+":"+port+")/"+dbname+"?tls=skip-verify&autocommit=true")
@@ -42,51 +40,51 @@ func insert() {
 
 }
 
-func selectQ() {
+// func selectQ() {
 
-	appConfig := &Config{}
-	if rawData, err := gonfig.Read(); err != nil {
-		panic(err)
-	} else {
-		if err := json.Unmarshal(rawData, appConfig); err != nil {
-			panic(err)
-		} else {
-			fmt.Printf(
-				"{\"name\": \"%s\", \"dbConfig\": {\"host\": \"%s\",\"dbname\": \"%s\", port: \"%d\", \"username\": \"%s\"}}\n",
-				appConfig.Name,
-				appConfig.DbConfig.Host,
-				appConfig.DbConfig.DatabaseName,
-				appConfig.DbConfig.Port,
-				appConfig.DbConfig.Username,
-			) // {"name": "new-awesome-name", "dbConfig": {"host": "prod-db-server", port: "1"}}
-		}
-	}
-	var (
-		firstname  string
-		lastname   string
-		title      string
-		department string
-	)
-	//fmt.Println(appConfig.DbConfig.Username + ":" + appConfig.DbConfig.Password + "@tcp(" + appConfig.DbConfig.Host + ":" + string(appConfig.DbConfig.Port) + ")/" + appConfig.DbConfig.DatabaseName + "?tls=skip-verify&autocommit=true")
-	db, err := sql.Open("mysql", appConfig.DbConfig.Username+":"+appConfig.DbConfig.Password+"@tcp("+appConfig.DbConfig.Host+":"+appConfig.DbConfig.Port+")/"+appConfig.DbConfig.DatabaseName+"?tls=skip-verify&autocommit=true")
-	if err != nil {
-		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
-	}
-	defer db.Close()
+// 	appConfig := &Config{}
+// 	if rawData, err := gonfig.Read(); err != nil {
+// 		panic(err)
+// 	} else {
+// 		if err := json.Unmarshal(rawData, appConfig); err != nil {
+// 			panic(err)
+// 		} else {
+// 			fmt.Printf(
+// 				"{\"name\": \"%s\", \"dbConfig\": {\"host\": \"%s\",\"dbname\": \"%s\", port: \"%d\", \"username\": \"%s\"}}\n",
+// 				appConfig.Name,
+// 				appConfig.DbConfig.Host,
+// 				appConfig.DbConfig.DatabaseName,
+// 				appConfig.DbConfig.Port,
+// 				appConfig.DbConfig.Username,
+// 			) // {"name": "new-awesome-name", "dbConfig": {"host": "prod-db-server", port: "1"}}
+// 		}
+// 	}
+// 	var (
+// 		firstname  string
+// 		lastname   string
+// 		title      string
+// 		department string
+// 	)
+// 	//fmt.Println(appConfig.DbConfig.Username + ":" + appConfig.DbConfig.Password + "@tcp(" + appConfig.DbConfig.Host + ":" + string(appConfig.DbConfig.Port) + ")/" + appConfig.DbConfig.DatabaseName + "?tls=skip-verify&autocommit=true")
+// 	db, err := sql.Open("mysql", appConfig.DbConfig.Username+":"+appConfig.DbConfig.Password+"@tcp("+appConfig.DbConfig.Host+":"+appConfig.DbConfig.Port+")/"+appConfig.DbConfig.DatabaseName+"?tls=skip-verify&autocommit=true")
+// 	if err != nil {
+// 		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+// 	}
+// 	defer db.Close()
 
-	rows, err := db.Query("SELECT * FROM employees")
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
-	defer rows.Close()
-	for rows.Next() {
-		err := rows.Scan(&firstname, &lastname, &title, &department)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println(firstname, lastname, title, department)
-	}
-}
+// 	rows, err := db.Query("SELECT * FROM employees")
+// 	if err != nil {
+// 		panic(err.Error()) // proper error handling instead of panic in your app
+// 	}
+// 	defer rows.Close()
+// 	for rows.Next() {
+// 		err := rows.Scan(&firstname, &lastname, &title, &department)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		log.Println(firstname, lastname, title, department)
+// 	}
+// }
 
 func delete() {
 
