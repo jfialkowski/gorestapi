b6b9ad7236f08de1999da1dd8c64e5ee2c8c71e8
jfialkowski <noreply@prgrmr.io>
2019-06-12 21:17:15 -0400

    testing2

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ func main() {
 	// 	}
 	// 	//pemBlocks = append(pemBlocks, crt)
 	// }
-	fmt.Printf("This should really print something1")
+	fmt.Println("This should really print something1")
 	var pkey []byte
 	encpkey, rest := pem.Decode(TLSKey)
 	fmt.Printf("Got a %T, with remaining data: %q", encpkey, rest)
