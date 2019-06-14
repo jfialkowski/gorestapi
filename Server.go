12530f64f1ab225509ddd3ad076c47ba2f70b9c5
jfialkowski <noreply@prgrmr.io>
2019-06-13 22:51:22 -0400

    Moved WebServer to own package

---
+++ Server.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func NewServer() http.Server {
+
+	router := NewRouter()
+
+	//Load Up The TLS Key and Cert to be used in HTTP Daemon
+	var pemBlocks []*pem.Block
+	var v *pem.Block
+	var cert *pem.Block
+	var pkey []byte
+
+	cert, rest := pem.Decode(TLSCert)
+	if cert == nil {
+		log.Fatal("Could not load certificate ")
+		fmt.Printf("%v", rest)
+	} else {
+		pemBlocks = append(pemBlocks, cert)
+	}
+	v, b := pem.Decode(TLSKey)
+	if v == nil {
+		log.Fatal("Could not load Private Key")
+		fmt.Printf("%v", b)
+
+	}
+	if v.Type == "PRIVATE KEY" {
+		if x509.IsEncryptedPEMBlock(v) {
+			pkey, _ = x509.DecryptPEMBlock(v, []byte(TLSPass))
+			pkey = pem.EncodeToMemory(&pem.Block{
+				Type:  v.Type,
+				Bytes: pkey,
+			})
+		} else {
+			fmt.Println("Encoded to memory")
+			pkey = pem.EncodeToMemory(v)
+		}
+	} else {
+		pemBlocks = append(pemBlocks, v)
+	}
+
+	//Encode Combined and decrypted key to memory
+	c, _ := tls.X509KeyPair(pem.EncodeToMemory(pemBlocks[0]), TLSKey)
+
+	// Construct a tls.config
+	cfg := &tls.Config{
+		MinVersion:               tls.VersionTLS12,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_AES_128_GCM_SHA256,
+			tls.TLS_AES_256_GCM_SHA384,
+			tls.TLS_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+		},
+		Certificates: []tls.Certificate{c},
+	}
+
+	// Build a server:
+	server := http.Server{
+		// Other options
+		TLSConfig:    cfg,
+		Handler:      router,
+		Addr:         ":9999",
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+	}
+	return server
+}
--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
 import (
-	"crypto/tls"
-	"crypto/x509"
-	"encoding/pem"
-	"fmt"
 	"log"
-	"net/http"
 	"os"
 )
 
@@ -14,89 +9,85 @@ func main() {
 
 	os.Setenv("AWS_SDK_LOAD_CONFIG", "true")
 
-	router := NewRouter()
+	//router := NewRouter()
 
 	//LoadConfig does just that, load your config
 	LoadConfig()
 
 	//ConnectDB connects to Database
 	ConnectDB(DBuser, DBpass, DBhost, DBport, DBname)
-	var pemBlocks []*pem.Block
-	var v *pem.Block
-	var cert *pem.Block
-	var pkey []byte
-	//var b []byte
-	//var rest []byte
-	fmt.Printf("TLSKey is %v \n", TLSKey)
 
-	cert, rest := pem.Decode(TLSCert)
-	if cert == nil {
-		fmt.Printf("Found something %v", rest)
-		log.Fatal("Could not load certificate ")
-	} else {
-		pemBlocks = append(pemBlocks, cert)
-	}
-	v, b := pem.Decode(TLSKey)
-	fmt.Printf("V is %v \n", v)
-	if v == nil {
-		fmt.Printf("Found something %v", b)
-		log.Fatal("Could not load Private Key")
-		//break
-	}
-	if v.Type == "PRIVATE KEY" {
-		fmt.Println("found private key")
-		if x509.IsEncryptedPEMBlock(v) {
-			pkey, _ = x509.DecryptPEMBlock(v, []byte(TLSPass))
-			pkey = pem.EncodeToMemory(&pem.Block{
-				Type:  v.Type,
-				Bytes: pkey,
-			})
-		} else {
-			fmt.Println("Encoded to memory")
-			pkey = pem.EncodeToMemory(v)
-		}
-	} else {
-		//fmt.Printf("found %v in rest \n ", b)
-		pemBlocks = append(pemBlocks, v)
-	}
-	//}
-	//Encode Combined and decrypted key to memory
-	fmt.Printf(" len=%d cap=%d Number of Index in pemBlocks \n", len(pemBlocks), cap(pemBlocks))
-	c, _ := tls.X509KeyPair(pem.EncodeToMemory(pemBlocks[0]), TLSKey)
+	//Load Up The TLS Key and Cert to be used in HTTP Daemon
+	// var pemBlocks []*pem.Block
+	// var v *pem.Block
+	// var cert *pem.Block
+	// var pkey []byte
 
-	// Construct a tls.config
-	cfg := &tls.Config{
-		MinVersion:               tls.VersionTLS12,
-		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
-		PreferServerCipherSuites: true,
-		CipherSuites: []uint16{
-			tls.TLS_AES_128_GCM_SHA256,
-			tls.TLS_AES_256_GCM_SHA384,
-			tls.TLS_CHACHA20_POLY1305_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_RSA_WITH_AES_128_CBC_SHA,
-			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-			tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
-		},
-		Certificates: []tls.Certificate{c},
-	}
+	// cert, rest := pem.Decode(TLSCert)
+	// if cert == nil {
+	// 	log.Fatal("Could not load certificate ")
+	// 	fmt.Printf("%v", rest)
+	// } else {
+	// 	pemBlocks = append(pemBlocks, cert)
+	// }
+	// v, b := pem.Decode(TLSKey)
+	// if v == nil {
+	// 	log.Fatal("Could not load Private Key")
+	// 	fmt.Printf("%v", b)
 
-	// Build a server:
-	server := http.Server{
-		// Other options
-		TLSConfig:    cfg,
-		Handler:      router,
-		Addr:         ":9999",
-		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
-	}
+	// }
+	// if v.Type == "PRIVATE KEY" {
+	// 	if x509.IsEncryptedPEMBlock(v) {
+	// 		pkey, _ = x509.DecryptPEMBlock(v, []byte(TLSPass))
+	// 		pkey = pem.EncodeToMemory(&pem.Block{
+	// 			Type:  v.Type,
+	// 			Bytes: pkey,
+	// 		})
+	// 	} else {
+	// 		fmt.Println("Encoded to memory")
+	// 		pkey = pem.EncodeToMemory(v)
+	// 	}
+	// } else {
+	// 	pemBlocks = append(pemBlocks, v)
+	// }
+
+	// //Encode Combined and decrypted key to memory
+	// c, _ := tls.X509KeyPair(pem.EncodeToMemory(pemBlocks[0]), TLSKey)
+
+	// // Construct a tls.config
+	// cfg := &tls.Config{
+	// 	MinVersion:               tls.VersionTLS12,
+	// 	CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+	// 	PreferServerCipherSuites: true,
+	// 	CipherSuites: []uint16{
+	// 		tls.TLS_AES_128_GCM_SHA256,
+	// 		tls.TLS_AES_256_GCM_SHA384,
+	// 		tls.TLS_CHACHA20_POLY1305_SHA256,
+	// 		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+	// 		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+	// 		tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+	// 		tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+	// 		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+	// 		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+	// 		tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+	// 		tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+	// 		tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+	// 		tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+	// 		tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+	// 		tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+	// 		tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+	// 	},
+	// 	Certificates: []tls.Certificate{c},
+	// }
+
+	// // Build a server:
+	// server := http.Server{
+	// 	// Other options
+	// 	TLSConfig:    cfg,
+	// 	Handler:      router,
+	// 	Addr:         ":9999",
+	// 	TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+	// }
+	server := NewServer()
 	log.Fatal(server.ListenAndServeTLS("", ""))
 }
