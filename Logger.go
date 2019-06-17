9296a36ff345cc0b4352efdefa52077ebdb6709c
jfialkowski <noreply@prgrmr.io>
2019-06-17 14:24:15 -0400

    Updates

--- Logger.go
+++ Logger.go
@@ -11,11 +11,18 @@ func Logger(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		inner.ServeHTTP(w, r)
+		IPAddress := r.Header.Get("X-Real-Ip")
+		if IPAddress == "" {
+			IPAddress = r.Header.Get("X-Forwarded-For")
+		}
+		if IPAddress == "" {
+			IPAddress = r.RemoteAddr
+		}
 		log.Printf(
 			"%s\t%s\t%s\t%s\t%s",
 			r.Method,
 			r.RequestURI,
-			r.Header.Get("X-Real-IP"),
+			IPAddress,
 			name,
 			time.Since(start),
 		)
