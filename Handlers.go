dbe3929ca4d0c653db2284bdb3622ecdeac7bbde
jfialkowski <noreply@prgrmr.io>
2019-06-17 12:28:44 -0400

    Updates

--- Handlers.go
+++ Handlers.go
@@ -79,6 +79,7 @@ func EmployeesDelete(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println("Decoding error: ", err)
 	}
+	fmt.Printf("Emp being sent over is: %v", &Emp)
 	result, err := DeleteEmployee(&Emp)
 	if err != nil {
 		log.Println(err)
